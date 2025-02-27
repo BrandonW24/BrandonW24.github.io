cd904bcf26852f388b3587fd782f9ef84919e563
Brandon Withington <brandon.f.withington@gmail.com>
2025-02-27 01:02:42 -0800

    Update video-carousel.js

--- assets/js/video-carousel.js
+++ assets/js/video-carousel.js
@@ -1,4 +1,6 @@
 const videos = [
+    { src : "https://www.youtube.com/watch?v=ekydofPcQHk", caption: "From Feb. 27th, 2025"},
+    { src: "https://www.youtube.com/watch?v=F_ZlCs3QVNY", caption: "From Feb. 24th, 2025" },
     { src: "https://www.youtube.com/embed/3-vVpFSisxE", caption: "From Feb. 19th, 2025" },
     { src: "https://www.youtube.com/embed/Hgjjfns1nXw", caption: "From Feb. 7, 2025" },
     { src: "https://www.youtube.com/embed/dnzlw88Q8FI", caption: "From Dec. 31, 2024" }
